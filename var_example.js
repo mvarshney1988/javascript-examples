fb67907cb5f802d83ca73f48d1929571c5fe69a2
Mohit <mvarshney1988@gmail.com>
2024-06-15 04:13:24 +0530

    Update var_example.js

--- var_example.js
+++ var_example.js
@@ -1,3 +1,4 @@
+// let scope is at block level and var scope is function level , means accessible anywhere in the function, even if its defined at block level
 function var_example(){
 if(true){
   var x=5, y=6;
