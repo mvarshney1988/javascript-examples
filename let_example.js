ce29cc9b2b0d382b3f794de437b5bdd427fc61c7
Mohit <mvarshney1988@gmail.com>
2024-06-15 04:13:00 +0530

    Update let_example.js

--- let_example.js
+++ let_example.js
@@ -1,5 +1,11 @@
-let x, y;
-function sum(x,y) {
+// let scope is at block level and var scope is function level , means accessible anywhere in the function, even if its defined at block level
+
+function let_example(flag) {
+if(flag) {
+  let x=5, y=6; 
+  return x+y;
+}
 return x+y;
 }
-console.log("sum="+ (x+y));
+console.log("sum="+ (let_example(true))); // 11
+console.log("sum="+(let_example(false)));// x, y undefined because "let" scope is at block level
