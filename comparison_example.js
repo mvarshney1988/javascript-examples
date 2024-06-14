9f7e0089a56d04e74312fef318fe13cedd993de9
Mohit <mvarshney1988@gmail.com>
2024-06-15 04:52:45 +0530

    Create comparison_example.js

---
+++ comparison_example.js
@@ -0,0 +1,6 @@
+let x = "ABM";
+let y = "ABN";
+let result = x < y;console.log(result); // true, string are compared ascii wise
+
+new String("a") == new String("a") // false
+new String("a") === new String("a") // true
