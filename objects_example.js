d09e5b4e73af4587be202b832434e4dba32f48c2
Mohit <mvarshney1988@gmail.com>
2024-06-15 07:41:12 +0530

    Update objects_example.js

--- objects_example.js
+++ objects_example.js
@@ -6,3 +6,11 @@ const person2 = new Object();
 person2.name="mohit";
 // two ways to access objects
 console.log(person.name+"-"+person["gender"])
+
+// add a property to existing object
+person.address={"hno":"123","pincode":12345}
+// access nested property
+
+console.log(person.address.hno)
+// delete a property from existing object
+delete person.address
