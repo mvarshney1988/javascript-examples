6543d2b696182be9203156ff59980eaf213b228b
Mohit <mvarshney1988@gmail.com>
2024-06-15 08:03:46 +0530

    Create string_example.js

---
+++ string_example.js
@@ -0,0 +1,13 @@
+// length
+
+// append
+
+// add quotes
+
+// intitialize , new operator slow down the process
+
+// compare 
+new String("mohit") == "mohit" // true
+new String("mohit") == new String("mohit") // false
+
+// template , without semicolon, new line , use ` at both sides
