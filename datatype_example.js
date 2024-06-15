42692b20d6a3534481eb7a18d58b21f4cfcfb265
Mohit <mvarshney1988@gmail.com>
2024-06-15 06:17:17 +0530

    Create datatype_example.js

---
+++ datatype_example.js
@@ -0,0 +1,9 @@
+// to create a BigInt, append n to the end of an integer or call BigInt():
+let x = BigInt(1234567890123456799012345) // 1234567890123456799012345n
+
+// JavaScript can only safely represent integers: Up to 9007199254740991 +(253-1) and Down to -9007199254740991 -(253-1)
+// array
+const cars = ["maruti", "audi", "BMW"];
+
+// Object:
+const name = {firstName:"John", lastName:"Doe"};
