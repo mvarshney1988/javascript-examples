3c0e7b280564b062ffd404c8cf2fd2aa14f4e758
Mohit <mvarshney1988@gmail.com>
2024-06-15 09:39:52 +0530

    Update string_example.js

--- string_example.js
+++ string_example.js
@@ -11,3 +11,22 @@ new String("mohit") == "mohit" // true
 new String("mohit") == new String("mohit") // false
 
 // template , without semicolon, new line , use ` at both sides
+
+
+// search can take regex but indexof doesnt
+let text = "main spain";
+text.search(/locate/);
+text.indexof("locate");
+
+// match local search/ global search
+
+text.match("ain");
+text.match(/ain/g);
+
+// matchAll() is an ES2020 feature. matchAll() does not work in Internet Explorer.
+
+// used like contains in string. includes() is an ES6 feature. includes() is not supported in Internet Explorer.
+let text = "Hello world";
+text.includes("world");
+
+// startsWith() and endsWith() is an ES6 feature.
