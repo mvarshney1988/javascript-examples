276f39c8612e4de5e37056441d946816f56e8ef3
Mohit <mvarshney1988@gmail.com>
2024-06-15 07:54:13 +0530

    Update objects_example.js

--- objects_example.js
+++ objects_example.js
@@ -1,9 +1,14 @@
-// three ways to define objects
+// four ways to define objects
 const person={"name":"mohit",gender:"male"}
 const person1 = {};
   person.name="mohit";
 const person2 = new Object();
 person2.name="mohit";
+function Person(name) {
+  this.name = name;
+  
+}
+const person3 = new Person("mohit")
 // two ways to access objects
 console.log(person.name+"-"+person["gender"])
 
@@ -14,3 +19,25 @@ person.address={"hno":"123","pincode":12345}
 console.log(person.address.hno)
 // delete a property from existing object
 delete person.address
+
+// adding method to object
+person.nameUpperCase = function () {
+  return person.name.toUpperCase();
+};
+
+// access method property of object using parenthesis ()
+console.log(person.nameUpperCase())
+
+// loop object properties
+for (let x in person) {
+console.log(person.x) // will not work
+  console.log(person[x])
+}
+
+// convert to array , values only / keys only/ all entries
+console.log(Object.entries(person))
+console.log(Object.keys(person))
+console.log(Object.values(person))
+
+// comvert to string
+console.log(JSON.stringify(person))
