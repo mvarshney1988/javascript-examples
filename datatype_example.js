91ce491c6349d20f6914f73ea3aed1511f6f4971
Mohit <mvarshney1988@gmail.com>
2024-06-15 06:21:26 +0530

    Update datatype_example.js

--- datatype_example.js
+++ datatype_example.js
@@ -3,7 +3,9 @@ let x = BigInt(1234567890123456799012345) // 1234567890123456799012345n
 
 // JavaScript can only safely represent integers: Up to 9007199254740991 +(253-1) and Down to -9007199254740991 -(253-1)
 // array
-const cars = ["maruti", "audi", "BMW"];
+const arr = ["maruti", "audi", "BMW"];
 
 // Object:
-const name = {firstName:"John", lastName:"Doe"};
+const obj = {firstName:"John", lastName:"Doe"};
+typeof(obj) // object
+typeof(arr) // array
