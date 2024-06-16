38b5a9e8ea65f1d415d08c102b877ddca34c9cf2
Mohit <mvarshney1988@gmail.com>
2024-06-16 13:32:36 +0530

    Create forloop_example.js

---
+++ forloop_example.js
@@ -0,0 +1,15 @@
+for (int i=0;i<10;i++) {
+console.log(i);
+}
+
+// for of loop
+let text= "test";
+for (let x of text){
+console.log(x); // print character 
+}
+
+// for in loop
+for (let x in text) {
+console.log(x); // print index
+}
+
