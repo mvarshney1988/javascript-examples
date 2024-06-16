ed0965253b2220e704e19033b94129d9c5510de1
Mohit <mvarshney1988@gmail.com>
2024-06-16 13:43:05 +0530

    Update forloop_example.js

--- forloop_example.js
+++ forloop_example.js
@@ -2,14 +2,32 @@ for (int i=0;i<10;i++) {
 console.log(i);
 }
 
-// for of loop
-let text= "test";
-for (let x of text){
-console.log(x); // print character 
-}
+
 
 // for in loop
 for (let x in text) {
 console.log(x); // print index
 }
 
+// for of loop
+let text= "test";
+for (let x of text){
+console.log(x); // print character 
+}
+
+// for of loop for array
+let personArr = new Array("person1","person2")
+for(let person of personArr){
+console.log(person);
+}
+//for of loop for Set, Set take square brackets
+let personsSet = new Set(["person1","person2"]);
+for(let person of personsSet) {
+console.log(person);
+}
+
+// for of loop for Map, Map take square brackets
+let persons = new Map([["person1", 100],["person2",200],["person3", 300]]);
+for(let person of persons) {
+console.log(person);
+}
