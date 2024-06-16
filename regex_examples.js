9eec2e56efa02ad1cb5df0bbcb8df46aa384a3ae
Mohit <mvarshney1988@gmail.com>
2024-06-16 14:42:46 +0530

    Update regex_examples.js

--- regex_examples.js
+++ regex_examples.js
@@ -6,4 +6,10 @@
 // [abc]	Find any of the characters between the brackets	[0-9]	Find any of the digits between the brackets
 let text = "this there";
 let result = text.match(/[th]/g);
-console.log(result);
+console.log(result); / print the occurrence
+
+// search for digit using \d
+
+let text = "123mohit"; 
+let result = text.match(/\d/g);
+console.log(result); // print the digit
