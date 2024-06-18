d287cfcfd8f6cf084f6e63b1d7191f1f921956a3
Mohit <mvarshney1988@gmail.com>
2024-06-18 07:53:10 +0530

    Update functions_example.js

--- functions_example.js
+++ functions_example.js
@@ -54,3 +54,20 @@ const name = {
 }
 let disp = person2.display.bind(name);
 console.log(disp());
+
+
+// closures - functions inside functions as self - invocations
+// counter problem, global counter variable can be modified by anywhere in the code, means accessible
+// put counter variable init inside the function, but it will always be init again and again
+// use self invocations as nested functions
+const add = function(){
+let counter =0;
+  return function(){
+     counter +=1;
+    return counter;
+  }
+}
+let counter = add();
+console.log(counter());
+console.log(counter());
+console.log(counter());
