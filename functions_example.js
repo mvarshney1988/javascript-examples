d0f05cb8f3322b1d9c6077a188c56eebe40be0ac
Mohit <mvarshney1988@gmail.com>
2024-06-18 07:12:58 +0530

    Update functions_example.js

--- functions_example.js
+++ functions_example.js
@@ -1,9 +1,56 @@
-//arrow functions
 
 let func = function sum(a,b){
 return a*b;
 }
 console.log(func(4,5));
 
+//arrow functions
+
 let func1 = (a,b) => a*b;
 console.log(func1(4,5));
+// call method for invokation
+
+const car = { wheels:4, engine: function(mode){
+return this.engineType+"-"+mode;
+}
+}
+const engines = {
+engineType:"Diesel"
+}
+console.log(car.engine.call(engines, "automatic"));
+// apply method for invokation
+// The call() method takes arguments separately. The apply() method takes arguments as an array.
+
+const bike = { wheels:2, engine: function(mode){
+return this.engineType+"-"+mode;
+}
+}
+const bikeEngines = {
+engineType:"Petrol"
+}
+console.log(bike.engine.apply(bikeEngines, ["manual"]));
+
+// bind is used for function borrowing and preventing losing this , likt timeout callback
+
+const person = {
+  firstName:"John",
+  display: function () {
+    return this.firstName;
+  }
+}
+
+let display = person.display.bind(person);
+setTimeout(console.log(display()), 3000); // even timeout here will prserve this
+
+// function borrow using bind
+
+const person2 = {
+  display: function () {
+    return this.firstName;
+  }
+}
+const name = {
+  firstName:"John"
+}
+let disp = person2.display.bind(name);
+console.log(disp());
